c46c997beea94851050ac54c5d23e6692694d53d
majidhussainqadri1-dot <majidhussainqadri1@gmail.com>
2026-08-04 12:11:43 +0500

    feat: type trusted donation financial facts

---
+++ src/Domain/DonationFinancialFactType.php
@@ -0,0 +1,12 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Sabri\CF03\Domain;
+
+enum DonationFinancialFactType: string
+{
+    case ONE_TIME_COMPLETED = 'donation.settled';
+    case MONTHLY_STARTED = 'donation.monthly_started';
+    case MONTHLY_CANCELLED = 'donation.monthly_cancelled';
+}
