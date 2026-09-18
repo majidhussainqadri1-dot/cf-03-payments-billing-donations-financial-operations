8de32082d672b6200d6032b38fee55729b51439f
majidhussainqadri1-dot <majidhussainqadri1@gmail.com>
2026-09-18 22:16:23 +0500

    Round 29: advance core carrier identity to 1.4.0-rc.4

--- src/Application/FutureExpansionRegistry.php
+++ src/Application/FutureExpansionRegistry.php
@@ -11,7 +11,7 @@ final class FutureExpansionRegistry
 {
     public const VERSION = '1.0';
     public const AMENDMENT_ID = 'CF03-FUTURE40-2026-09-08';
-    public const SOFTWARE_TARGET = '1.4.0-rc.3';
+    public const SOFTWARE_TARGET = '1.4.0-rc.4';
 
     /**
      * These are coded future capabilities, not proof of staging, Live or operational activation.
